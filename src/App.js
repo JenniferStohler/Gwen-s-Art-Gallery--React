d5664870e910a57ec928d3e6d150964cd49373af
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-09-08 13:31:31 -0600

    getting the first image to show up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="container">
-
+        <div className="container">
+        <img className='img h-4/5'src='/Images/agent_hellboy_by_spectratherestless-d6nx6h1.jpg' alt='' />
         </div>
       </header>
     </div>
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+
+const ImageCard = () => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    </div>
+  )
+}
+
+export default ImageCard;
